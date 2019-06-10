2dc2ae574d96653d0df9cff38a645ff9af2e3d1e
suwignyo <gsuwignyo@gmail.com>
2019-06-09 22:31:13 -0400

    updated react-navigation, updated MainActivity.java for gesturehandler

--- src/modules/navigation/SideMenuContainer.js
+++ src/modules/navigation/SideMenuContainer.js
@@ -1,57 +1,56 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 // components
-import SideMenu from './components/SideMenu';
+import SideMenu from "./components/SideMenu";
 
 // actions
-import { changeScene } from './navigationActions';
+import { changeScene } from "./navigationActions";
 
 // other module actions
-import { logout } from '../auth/authActions';
+import { logout } from "../auth/authActions";
 
 class SideMenuContainer extends Component {
-    static propTypes = {
-        // states
-        me: PropTypes.object,
+  static propTypes = {
+    // states
+    me: PropTypes.object,
 
-        // actions
-        changeScene: PropTypes.func.isRequired,
+    // actions
+    changeScene: PropTypes.func.isRequired,
 
-        // other module actions
-        logout: PropTypes.func.isRequired,
-    };
+    // other module actions
+    logout: PropTypes.func.isRequired,
+  };
 
-    render() {
-        const {me, changeScene, navigation, logout} = this.props;
-        const sceneKey = navigation.getParam('sceneKey');
-
-        return (
-            <SideMenu
-                me={me}
-                sceneKey={sceneKey}
-                changeScene={changeScene}
-                logout={logout}
-            />
-        );
-    }
+  render() {
+    const { me, changeScene, navigation, logout } = this.props;
+    const sceneKey = navigation.getParam("sceneKey");
+    return (
+      <SideMenu
+        me={me}
+        sceneKey={sceneKey}
+        changeScene={changeScene}
+        logout={logout}
+      />
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        // states
-        me: state.me.me,
-    };
+  return {
+    // states
+    me: state.me.me,
+  };
 }
 
 export default connect(
-    mapStateToProps,
-    {
-        // actions
-        changeScene,
+  mapStateToProps,
+  {
+    // actions
+    changeScene,
 
-        // other module actions
-        logout,
-    }
+    // other module actions
+    logout,
+  },
 )(SideMenuContainer);
